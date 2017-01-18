ed9e28f6c8b4fadecaf21f5f644edf1ad121c8ff
Maxwell Talley <max.talley0@gmail.com>
2017-01-18 03:01:00 -0600

    Mostly done
    All done except for an issue with the lightweight spaceships becoming a
    four cell block and never colliding. Recursion was not successful.

--- game_of_life.c
+++ game_of_life.c
@@ -1,102 +1,269 @@
-#include "game_of_life.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
 
-#define MIN(x, y)  (((x) > (y)) ? (x) : (y))
-#define MAX(x, y)  (((x) < (y)) ? (x) : (y))
+int counter[24][80];
+char world[24][80];
+char temp[24][80];
 
-int counter(char *world[80]);
-//in a 2D array, rows come first, then cols.
+void count(int row, int col);
+void rules();
 
 int main(int argc, char *argv[]){
-    //swap
-    //char world[80][24];
-    char world[24][80];
     int xStart = 0;
     int yStart = 0;
-    int frameCount = 1;
-    //swap
-    //int counter[80][24];
-    int counter[24][80];
-    
+    int frame = 1;
+
     for(int i = 1; i < argc; i++)
     {
         xStart = atoi(argv[i]);
         i++;
         yStart = atoi(argv[i]);
-        world[xStart][yStart] = 'O';
+        world[yStart][xStart] = 'O';
         xStart = 0; yStart = 0;
     }
-    
-    //swap x and y for loop
+
     for(int y = 0; y < 24; y++)
     {
         for(int x = 0; x < 80; x++)
         {
-            //swap
             if(world[y][x] != 'O')
-            {
                 world[y][x] = ' ';
-            }
-            
-            printf("%c", world[x][y]);
-            
+
+            // printf("%c", world[y][x]);
+            count(y, x);
+            temp[y][x] = world[y][x];
         }
-        printf("\n");
     }
-    
     while(1) {
-        usleep(83333);
-        
-        //swap x and y in for loop
+        //usleep(83333);
+        usleep(166666);
         for(int y = 0; y < 24; y++)
         {
             for(int x = 0; x < 80; x++)
             {
-                //swap
-                if(world[y][x] != 'O')
-                {
-                    world[y][x] = ' ';
-                }
-                
-                //swap
                 printf("%c", world[y][x]);
-                
+                //printf("%2d", counter[y][x]);
+                count(y,x);
             }
             printf("\n");
         }
-    
-        counter(world, counter);
-        
+        rules();
+        memcpy(world, temp, sizeof(world));
+        printf("%d\n",frame++);
     }
-    
-    
+
+    // for(int i = 0; i < 24; i++)
+    //   for(int j = 0; j < 80; j++)
+    //     count(i, j);
+    //
+    // for(int row = 0; row < 24; row++) {
+    //   for(int col = 0; col < 80; col++) {
+    //     printf("%2d", counter[row][col]);
+    //   }
+    //   printf("\n");
+    // }
+
+
     return 0;
-    
 }
 
 
-void counter(char *world[80], int *counter[80])
+void count(int row, int col)
 {
-    //swap
-    int cellCount[24][80];
-    for(int m = 0; m < 24; m++)
-    {
-        for(int n = 0; n < 80; n++)
-        {
-            for(int i = MAX(0, m - 1); i <= MIN(m - 1, m + 1); i++)
-            {
-                for(int j = MAX(0, n - 1); j <= MIN(n - 1, n + 1); j++)
-                {
-                    //swap
-                    //y, x
-                    if(world[i][j] == 'O')
-                        counter[n][m]++;
-                }
-            }
+  int count = 0;
+  //top left
+  if(row == 0 && col == 0)
+  {
+    if(world[0][79] == 'O')
+      count++;
+    if(world[1][79] == 'O')
+      count++;
+    if(world[23][79] == 'O')
+      count++;
+    if(world[23][78] == 'O')
+      count++;
+    if(world[23][77] == 'O')
+      count++;
+    if(world[0][1] == 'O')
+      count++;
+    if(world[1][0] == 'O')
+      count++;
+    if(world[1][1] == 'O')
+      count++;
+  }
+  //top right
+  else if(row == 0 && col == 79)
+  {
+    if(world[23][78] == 'O')
+      count++;
+    if(world[23][79] == 'O')
+      count++;
+    if(world[23][0] == 'O')
+      count++;
+    if(world[0][78] == 'O')
+      count++;
+    if(world[0][0] == 'O')
+      count++;
+    if(world[1][78] == 'O')
+      count++;
+    if(world[1][79] == 'O')
+      count++;
+    if(world[1][0] == 'O')
+      count++;
+  }
+  //bottom left
+  else if(row == 23 && col == 0)
+  {
+    if(world[22][79] == 'O')
+      count++;
+    if(world[22][0] == 'O')
+      count++;
+    if(world[22][1] == 'O')
+      count++;
+    if(world[22][79] == 'O')
+      count++;
+    if(world[22][1] == 'O')
+      count++;
+    if(world[0][79] == 'O')
+      count++;
+    if(world[0][0] == 'O')
+      count++;
+    if(world[0][1] == 'O')
+      count++;
+  }
+  //bottom right
+  else if(row == 23 && col == 79)
+  {
+    if(world[22][78] == 'O')
+      count++;
+    if(world[22][79] == 'O')
+      count++;
+    if(world[22][0] == 'O')
+      count++;
+    if(world[23][78] == 'O')
+      count++;
+    if(world[23][0] == 'O')
+      count++;
+    if(world[0][78] == 'O')
+      count++;
+    if(world[0][79] == 'O')
+      count++;
+    if(world[0][0] == 'O')
+      count++;
+  }
+  //0 column
+  else if(col == 0)
+  {
+    if(world[row + 1][79] == 'O')
+      count++;
+    if(world[row + 1][0] == 'O')
+      count++;
+    if(world[row + 1][1] == 'O')
+      count++;
+    if(world[row][79] == 'O')
+      count++;
+    if(world[row][1] == 'O')
+      count++;
+    if(world[row - 1][79] == 'O')
+      count++;
+    if(world[row - 1][0] == 'O')
+      count++;
+    if(world[row - 1][1] == 'O')
+      count++;
+  }
+  //23 column
+  else if(col == 79)
+  {
+    if(world[row + 1][78] == 'O')
+      count++;
+    if(world[row + 1][79] == 'O')
+      count++;
+    if(world[row + 1][0] == 'O')
+      count++;
+    if(world[row][78] == 'O')
+      count++;
+    if(world[row][0] == 'O')
+      count++;
+    if(world[row - 1][78] == 'O')
+      count++;
+    if(world[row - 1][79] == 'O')
+      count++;
+    if(world[row - 1][0] == 'O')
+      count++;
+  }
+  //0 row
+  else if(row == 0)
+  {
+    if(world[23][col - 1] == 'O')
+      count++;
+    if(world[23][col] == 'O')
+      count++;
+    if(world[23][col + 1] == 'O')
+      count++;
+    if(world[0][col - 1] == 'O')
+      count++;
+    if(world[0][col + 1] == 'O')
+      count++;
+    if(world[1][col - 1] == 'O')
+      count++;
+    if(world[1][col] == 'O')
+      count++;
+    if(world[1][col + 1] == 'O')
+      count++;
+  }
+  //79 row
+  else if(row == 23)
+  {
+    if(world[78][col - 1] == 'O')
+      count++;
+    if(world[78][col] == 'O')
+      count++;
+    if(world[78][col + 1] == 'O')
+      count++;
+    if(world[79][col - 1] == 'O')
+      count++;
+    if(world[79][col + 1] == 'O')
+      count++;
+    if(world[0][col - 1] == 'O')
+      count++;
+    if(world[0][col] == 'O')
+      count++;
+    if(world[0][col + 1] == 'O')
+      count++;
+  }
+  //non edge case
+  else
+  {
+    for(int i = row - 1; i < row + 2; i++) {
+      for(int j = col - 1; j <  col + 2; j++) {
+        if(world[i][j] == 'O') {
+          if(i == row && j == col) {}
+          else count++;
         }
+      }
     }
+  }
+  counter[row][col] = count;
+}
+
+void rules() {
+  for(int y = 0; y < 24; y++)
+  {
+    for(int x = 0; x < 80; x++)
+    {
+      if((counter[y][x] == 0 || counter[y][x] == 1) && world[y][x] == 'O')
+        temp[y][x] = ' ';
+
+      else if((counter[y][x] == 2 || counter[y][x] == 3) && world[y][x] == 'O') { }
+
+      else if(counter[y][x] >= 4 && world[y][x] == 'O')
+        temp[y][x] = ' ';
+
+      else if(counter[y][x] == 3 && world[y][x] == ' ')
+        temp[y][x] = 'O';
+    }
+  }
 }
