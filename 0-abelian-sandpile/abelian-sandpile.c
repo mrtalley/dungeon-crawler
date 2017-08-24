9e1720f7581c0c64b43cdf7a6ec1b04548d196a1
mrtalley <mrtalley@iastate.edu>
2017-08-24 14:20:53 -0500

    AS-3 Fill map with command line values

--- 0-abelian-sandpile/abelian-sandpile.c
+++ 0-abelian-sandpile/abelian-sandpile.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+void fillMapWithCommandLineValues(int map[23][23], int argc, char *argv[]);
 void dropSand(int map[23][23]);
 void toppleSand(int map[23][23]);
 
@@ -10,6 +11,13 @@ int main(int argc, char *argv[])
     int map[23][23] = {{0}};
     int x = 0;
     int y = 0;
+
+    if(argc - 1 % 3 == 0 && argc != 1) {
+        printf("%s\n", "Please put parameters on the command line in form of y x h");
+        return 0;
+    }
+
+    fillMapWithCommandLineValues(map, argc, argv);
 
     while(1) {
         usleep(83333);
@@ -25,6 +33,19 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+void fillMapWithCommandLineValues(int map[23][23], int argc, char *argv[])
+{
+    int i = 0, x = 0, y = 0, h = 0;
+
+    for(i = 1; i < argc; i++) {
+        y = atoi(argv[i]); i++;
+        x = atoi(argv[i]); i++;
+        h = atoi(argv[i]);
+
+        map[x][y] = h;
+    }
+}
+
 void dropSand(int map[23][23])
 {
     map[11][11] += 1;
@@ -37,7 +58,7 @@ void handleSand(int map[23][23])
 
     for(y = 0; y < 23; y++) {
         for(x = 0; x < 23; x++) {
-            
+
         }
     }
 }
