1d826c7a2b0f52d86340a29610b4e5dcd5067c92
mrtalley <mrtalley@iastate.edu>
2017-10-31 21:36:13 -0500

    Initial commit of 1.07

---
+++ 1.07-monster-definitions/fileio.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <sstream> // string stream
+#include <string>
+
+#include "fileio.h"
+
+void parse_monster_file()
+{
+    string home = getenv("HOME");
+    ifstream f("home/.rlg327/monster_desc.txt");
+}
---
+++ 1.07-monster-definitions/pc.h
@@ -0,0 +1,20 @@
+#ifndef PC_H
+# define PC_H
+
+# include <stdint.h>
+
+# include "dungeon.h"
+
+typedef struct dungeon dungeon_t;
+
+class pc : public character {
+};
+
+void pc_delete(pc_t *pc);
+uint32_t pc_is_alive(dungeon_t *d);
+void config_pc(dungeon_t *d);
+uint32_t pc_next_pos(dungeon_t *d, pair_t dir);
+void place_pc(dungeon_t *d);
+uint32_t pc_in_room(dungeon_t *d, uint32_t room);
+
+#endif
